f4dde964a86928c855c8fde07bfaeb861b0f4b11
emf-99 <113733097+emf-99@users.noreply.github.com>
2024-01-23 12:22:37 -0500

    :tada: theme, initial commit

---
+++ idm250-emf99-theme/font-page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>
+   <?php
+    echo get_the_title();
+   ?>
+</h1>
+
+<p> this is a font-page.php file </p>
+
+</body>
+</html>
---
+++ idm250-emf99-theme/index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<h1>
+   <?php
+    echo get_the_title();
+   ?>
+</h1>
+
+<p> this is a page.php file </p>
+
+<?php get_footer(); ?>
