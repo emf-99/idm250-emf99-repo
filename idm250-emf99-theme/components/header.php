46eb9a9c42cb351b719a54558baa56e7dc62c4f6
emf-99 <113733097+emf-99@users.noreply.github.com>
2024-02-12 18:42:27 -0500

    :art: corrections

--- idm250-emf99-theme/components/header.php
+++ idm250-emf99-theme/components/header.php
@@ -1,4 +1,6 @@
-<header class="border border-black px-4 py-2 text-right"> 
+<header> 
+
+<img class="header-logo" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/logo.png" alt="logo" class="w-30 h-16">
 
 <?php wp_nav_menu([
     'theme_location' => 'primary'
--- idm250-emf99-theme/footer.php
+++ idm250-emf99-theme/footer.php
@@ -1,6 +1,13 @@
-<footer class="flex items-center justify-center">
+<footer>
 
-    <p class="mr-2">&copy <?php echo date('Y'); ?> | idm250 | ella fromherz</p>
+    <div class="footer-text">
+        <p class="date-footer"> &copy <?php echo date('Y'); ?></p>
+        <p class="class-footer"> idm250 </p>
+        <p class="name-footer"> ella fromherz </p>
+    </div>
+
+    <img class="footer-logo" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/logo.png" alt="logo">
+
     <?php wp_nav_menu([
     'theme_location' => 'footer'
 ]); ?>
